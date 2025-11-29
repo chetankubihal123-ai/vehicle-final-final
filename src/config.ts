49f08a01a6af381735c5513b40efd998cb616169
chetankubihal123-ai <chetankubihal123@gmail.com>
2025-11-29 19:03:26 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // API Configuration
 // Using local IP address for mobile testing
 //export const API_URL = "http://localhost:5000/api";
-export const API_URL = import.meta.env.VITE_API_URL || "https://vehicle-final-final.onrender.com/api";
+export const API_URL = import.meta.env.VITE_API_URL || "https://vehicle-final-final.onrender.com";
 // WebSocket URL
 export const WS_URL = API_URL;
 
