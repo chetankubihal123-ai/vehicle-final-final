351ec128d7c99bd4208dd8372fa5217d78db0fd5
chetankubihal123-ai <chetankubihal123@gmail.com>
2025-12-02 20:23:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -49,14 +49,17 @@ app.get("/", (req, res) => {
 });
 
 // HTTP server + Socket.io
-const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: [
+      "https://vehicle-frontend-tuhe.onrender.com",
+      "http://localhost:5173"
+    ],
     methods: ["GET", "POST"],
-    credentials: true,
-  },
+    credentials: true
+  }
 });
+
 
 // Models
 const LiveLocation = require("./models/LiveLocation");
